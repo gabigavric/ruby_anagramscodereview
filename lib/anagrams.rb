b31227afef2d9ce22c48949e68bb2f6efeda5716
gabrijela <gabigavric@gmail.com>
2017-09-08 16:47:35 -0700

    return your input and make it lowercase but only the first word

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,30 +1,14 @@
 #!/usr/bin/env ruby
 
-class Words
+class Words < String
   def initialize(user_input)
     @user_input =  user_input
+
+
   end
 
-  def convert_to_lowercase(user_input)
+  def convert(user_input)
     user_input = user_input.downcase()
+    array.push(user_input)
   end
 end
-#   def voul_check()
-#   end
-#
-#   def word_check_for_palindroms()
-#   end
-#
-#   def check_for_anagrams_word()
-#   end
-#
-#   def check_for_antigrams_word()
-#   end
-#
-#   def check_for_anagrams_sentance()
-#   end
-#
-#   def check_for_antigrams_sentance()
-#   end
-#
-# end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,8 +2,13 @@ require('rspec')
 require('anagrams')
 
 describe('#anagrams') do
-  cats = Words.new('cat')
+  new_instance1 = Words.new('user_input')
+
   it("Convert any uppercase letters to lowercase.") do
-      expect(cats.convert_to_lowercase("Cat")).to(eq("cat"))
+      expect(new_instance1.convert("Cat")).to(eq("cat"))
+  end
+
+  it("Push user input into an array.") do
+      expect(new_instance1.convert("cat", "hat")).to(eq["cat, hat"])
   end
 end

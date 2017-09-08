2279a621318c3e720cff361fbc6f828de4cf8679
gabrijela <gabigavric@gmail.com>
2017-09-08 15:01:47 -0700

    pass the first test, change any letters string from uppercase to lowercase

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,1 +1,30 @@
 #!/usr/bin/env ruby
+
+class Words
+  def initialize(user_input)
+    @user_input =  user_input
+  end
+
+  def convert_to_lowercase(user_input)
+    user_input = user_input.downcase()
+  end
+end
+#   def voul_check()
+#   end
+#
+#   def word_check_for_palindroms()
+#   end
+#
+#   def check_for_anagrams_word()
+#   end
+#
+#   def check_for_antigrams_word()
+#   end
+#
+#   def check_for_anagrams_sentance()
+#   end
+#
+#   def check_for_antigrams_sentance()
+#   end
+#
+# end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,39 +1,9 @@
 require('rspec')
 require('anagrams')
-require('pry')
 
-describe('#methodname') do
-
-  it("Check inputed words for the following vouls: 'a', 'e', 'i', 'o', 'u'.")
-    expect(("cat","lick")).to(eq("The inputed words contain vouls"))
+describe('#anagrams') do
+  cats = Words.new('cat')
+  it("Convert any uppercase letters to lowercase.") do
+      expect(cats.convert_to_lowercase("Cat")).to(eq("cat"))
   end
-
-  it("Check to see if inputs are palindroms.") do
-    expect(("bob", "bob")).to(eq("Your words are palindroms"))
-  end
-
-  it("Check to see if inputs are anagrams.") do
-    expect(("cinema", "iceman")).to(eq("Your inputs are anagrams"))
-  end
-
-  it("Check to see if inputs are antigrams.") do
-    expect(("hi","bye")).to(eq("Your inputs are antigrams"))
-  end
-
-  it("Check to see if a sentance is an anagram.") do
-    expect(("nurses run")).to(eq("Your sentance is an anagram."))
-  end
-
-  it("Check to see if a sentance is an antigram.") do
-    expect(("purses run")).to(eq("Your sentance is an antigram."))
-  end
-
-
-
 end
-
-    # describe('#methodname') do
-    #
-    #   it("check to see if movie is an anagram") do
-    #     expect(paperEx.wins("paper")).to(eq("Tie"))
-    #   end

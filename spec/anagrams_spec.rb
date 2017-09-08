bf06967b6a4239802906fad92e726ca32513f2c7
gabrijela <gabigavric@gmail.com>
2017-09-08 12:38:09 -0700

    make initial repository so i dont lose changes

---
+++ spec/anagrams_spec.rb
@@ -0,0 +1,20 @@
+require('rspec')
+require('anagrams')
+require('pry')
+
+describe('#methodname') do
+
+  it("check to see if the word bob is an anagram") do
+    expect(("bob")).to(eq("bob"))
+  end
+
+  it("check to see if movie is an anagram") do
+      expect(paperEx.wins("paper")).to(eq("Tie"))
+  end
+
+
+    # describe('#methodname') do
+    #
+    #   it("check to see if movie is an anagram") do
+    #     expect(paperEx.wins("paper")).to(eq("Tie"))
+    #   end

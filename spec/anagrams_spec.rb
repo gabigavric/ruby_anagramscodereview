d74595a38398e4257e7afc98b9f9cd29de7e5bd7
gabrijela <gabigavric@gmail.com>
2017-09-08 13:26:13 -0700

    update rspec file for errors

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,28 +5,54 @@ require('pry')
 describe('#methodname') do
 
   it("Check inputed words for the following vouls: 'a', 'e', 'i', 'o', 'u'.")
-    expect(("cat","cat")).to(eq("true"))
+    expect(("cat","lick")).to(eq("The inputed words contain vouls"))
   end
 
   it("Check inputed words for the following vouls: 'a', 'e', 'i', 'o', 'u'.")
-    expect(("ct","ct")).to(eq("false"))
+    expect(("ct","ct")).to(eq("The inputed words do not contain vouls"))
+  end
+
+  it("Check to see if inputs are palindroms.") do
+    expect(("bob", "bob")).to(eq("Your words are palindroms of eachother"))
+  end
+
+  it("Check to see if inputs are palindroms.") do
+    expect(("bob","rob")).to(eq("Your words are not palindroms of eachother"))
   end
 
   it("Check to see if inputs are anagrams.") do
-    expect(("cinema", "iceman")).to(eq("true"))
+    expect(("cinema", "iceman")).to(eq("Your words are anagrams of eachother"))
   end
 
   it("Check to see if inputs are anagrams.") do
-    expect(("movie","movie" )).to(eq("false"))
+    expect(("movie","movie")).to(eq("Your words are not anagrams of eachother"))
   end
 
   it("Check to see if inputs are antigrams.") do
-    expect(("hi","bye" )).to(eq("true"))
+    expect(("hi","bye")).to(eq("true"))
   end
 
   it("Check to see if inputs are antigrams.") do
-    expect(("house","mouse" )).to(eq("false"))
+    expect(("house","mouse")).to(eq("false"))
   end
+
+  it("Check to see if a sentance is an anagram.") do
+    expect((" ")).to(eq("true"))
+  end
+
+  it("Check to see if a sentance is an anagram.") do
+    expect((" ")).to(eq("false"))
+  end
+
+  it("Check to see if a sentance is an antigram.") do
+    expect((" ")).to(eq("true"))
+  end
+
+  it("Check to see if a sentance is an antigram.") do
+    expect((" ")).to(eq("false"))
+  end
+
+
 
 end
 

8d0d2eaa0ee21af89ebe292c0ffcf0cce3c74057
gabrijela <gabigavric@gmail.com>
2017-09-08 13:13:32 -0700

    make spec file almost complete just need to make lib so i can enter method name

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,14 +4,31 @@ require('pry')
 
 describe('#methodname') do
 
-  it("check to see if the word bob is an anagram") do
-    expect(("bob")).to(eq("bob"))
+  it("Check inputed words for the following vouls: 'a', 'e', 'i', 'o', 'u'.")
+    expect(("cat","cat")).to(eq("true"))
   end
 
-  it("check to see if movie is an anagram") do
-      expect(paperEx.wins("paper")).to(eq("Tie"))
+  it("Check inputed words for the following vouls: 'a', 'e', 'i', 'o', 'u'.")
+    expect(("ct","ct")).to(eq("false"))
   end
 
+  it("Check to see if inputs are anagrams.") do
+    expect(("cinema", "iceman")).to(eq("true"))
+  end
+
+  it("Check to see if inputs are anagrams.") do
+    expect(("movie","movie" )).to(eq("false"))
+  end
+
+  it("Check to see if inputs are antigrams.") do
+    expect(("hi","bye" )).to(eq("true"))
+  end
+
+  it("Check to see if inputs are antigrams.") do
+    expect(("house","mouse" )).to(eq("false"))
+  end
+
+end
 
     # describe('#methodname') do
     #

7906ec13dd20ab23f36b7a2d06b530c4c81ba4ff
Guest User <Guest@Epicodus-4B.local>
2017-09-12 18:12:46 -0700

    installed gems and fixed lib

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 
-class Words < String
-  def initialize(user_input)
-    @user_input =  user_input
-
-
+class Words
+  def initialize(input1, input2)
+    @input1 = input1
+    @input2 = input2
   end
 
-  def convert(user_input)
-    user_input = user_input.downcase()
-    array.push(user_input)
+  def convert()
+    word1 = @input1.downcase().split(//)
+    word2 = @input2.downcase().split(//)
+
+    word1.chars().sort()
+    word2.chars().sort()
+
+    word1.char.sort ==  word2.char.sort
   end
 end
